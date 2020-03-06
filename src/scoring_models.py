c388448ba5a98515ff8b5f4c8592382c0840d176
chiffa <andrei.chiffa136@gmail.com>
2020-03-06 09:16:05 +0100

    finalized the Arena logic

--- src/scoring_models.py
+++ src/scoring_models.py
@@ -30,9 +30,10 @@ def log_weighted_elo(elo_A, elo_B, A_lead, base_k=16):
                         factor_k_function=multiplier_function)
 
 
+
 def pathogen_host_fitness(real_av_error, fake_av_error,
                           autoimmunity_factor=20, virulence_factor=20,
-                          effective_phenotype_space_dimenstions=80):
+                          effective_phenotype_space_dimensions=80):
     """
     The reason that we are using Weibull is due to the existence of the historical reasons of
     the Fisherian phenotypic model.
@@ -44,11 +45,30 @@ def pathogen_host_fitness(real_av_error, fake_av_error,
 
     """
 
-    host_fitness = stats.weibull_min.cdf(real_av_error * autoimmunity_factor
-                                 + fake_av_error * virulence_factor,
-                                 effective_phenotype_space_dimenstions)
+    cumulative_load = np.sqrt(np.power(real_av_error * autoimmunity_factor, 2)
+                                 + np.power(fake_av_error * virulence_factor, 2))
 
-    pathogen_fitness = stats.weibull_min.cdf((1-fake_av_error) * virulence_factor*2,
-                                 effective_phenotype_space_dimenstions)
+    host_fitness = stats.weibull_min.cdf(cumulative_load, effective_phenotype_space_dimensions)
+
+    pathogen_fitness = virulence_factor * stats.weibull_min.cdf((1-fake_av_error) * virulence_factor * 2,
+                                                                effective_phenotype_space_dimensions)
 
     return host_fitness, pathogen_fitness
+
+
+def cumulative_host_fitness(real_av_error, fake_av_error_vector,
+                        autoimmunity_factor=20, virulence_factor_vector=[],
+                        effective_phenotype_space_dimensions=80):
+
+    if len(virulence_factor_vector) == 0:
+        virulence_factor_vector = [20 for _ in fake_av_error_vector]
+
+    cumulative_load = np.sum(np.array([(av_err*vir_fac)**2 for av_err, vir_fac in
+                                         zip(fake_av_error_vector, virulence_factor_vector)]))
+
+    cumulative_load += np.power(real_av_error * autoimmunity_factor, 2)
+    cumulative_load = np.sqrt(cumulative_load)
+
+    host_fitness = stats.weibull_min.cdf(cumulative_load, effective_phenotype_space_dimensions)
+
+    return host_fitness
