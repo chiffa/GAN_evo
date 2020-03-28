2c01f9a8b75ecf04eeac22fda39b69bf17bc2736
chiffa <andrei.chiffa136@gmail.com>
2020-03-28 21:50:09 +0100

    added logic for incremental fid computing

--- src/fid_analyser.py
+++ src/fid_analyser.py
@@ -11,37 +11,53 @@ balancing_folders_location = '/home/kucharav/FID_samples'
 fid_command = '/home/kucharav/Documents/pytorch-fid-master/fid_score.py'
 after_datetime = datetime.datetime.now() - datetime.timedelta(days=1)
 
-random_tag_list = []
-fid_map = {}
-real_comparison = []
 
-for random_tag in os.listdir(balancing_folders_location):
-    random_tag_list.append(random_tag)
-    current_real = balancing_folders_location + '/' + random_tag + '/' + 'real'
-    current_fake = balancing_folders_location + '/' + random_tag + '/' + 'fake'
+def calc_gen_fids():
+    random_tag_list = []
+    fid_map = {}
+    for random_tag in os.listdir(balancing_folders_location):
+        if os.path.getmtime(random_tag) > after_datetime:
+            random_tag_list.append(random_tag)
+            current_real = balancing_folders_location + '/' + random_tag + '/' + 'real'
+            current_fake = balancing_folders_location + '/' + random_tag + '/' + 'fake'
 
-    try:
-        fid_value = calculate_fid_given_paths([current_real, current_fake], 64, True, 2048)
-        fid_map[random_tag] = fid_value
-        print(random_tag, ': ', fid_value)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
+            try:
+                fid_value = calculate_fid_given_paths([current_real, current_fake], 64, True, 2048)
+                fid_map[random_tag] = fid_value
+                print(random_tag, ': ', fid_value)
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
 
-shuffle(random_tag_list)
-
-blocker = 20
-for i, (random_tag_1, random_tag_2) in enumerate(combinations(random_tag_list, 2)):
-    if i > blocker:
-        break
-    current_1 = balancing_folders_location + '/' + random_tag_1 + '/' + 'real'
-    current_2 = balancing_folders_location + '/' + random_tag_2 + '/' + 'real'
-
-    try:
-        fid_value = calculate_fid_given_paths([current_1, current_2], 64, True, 2048)
-        real_comparison.append(fid_value)
-        print('real to real sample', ': ', fid_value)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
+    return fid_map, random_tag_list
 
 
-pickle.dump((fid_map, real_comparison), open('fid_scores.dmp', 'wb'))
+def calc_reals_fid(random_tag_list):
+    real_comparison = []
+    shuffle(random_tag_list)
+    blocker = 20
+    for i, (random_tag_1, random_tag_2) in enumerate(combinations(random_tag_list, 2)):
+        if i > blocker:
+            break
+        current_1 = balancing_folders_location + '/' + random_tag_1 + '/' + 'real'
+        current_2 = balancing_folders_location + '/' + random_tag_2 + '/' + 'real'
+
+        try:
+            fid_value = calculate_fid_given_paths([current_1, current_2], 64, True, 2048)
+            real_comparison.append(fid_value)
+            print('real to real sample', ': ', fid_value)
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+
+    return real_comparison
+
+if __name__ == "__main__":
+    fid_map, random_tag_list = calc_gen_fids()
+    real_comparison = calc_reals_fid(random_tag_list)
+
+    if os.path.isfile('fid_scores.dmp'):
+        old_fid_map, old_real_comparison = pickle.load((fid_map, real_comparison),
+                                                       open('fid_scores.dmp', 'rb'))
+        fid_map.update(old_fid_map)
+        real_comparison += old_real_comparison
+
+    pickle.dump((fid_map, real_comparison), open('fid_scores.dmp', 'wb'))
