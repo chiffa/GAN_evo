ab0198978f1eb01583b8e2d800498d9f89768284
Kevin Blin <kblin@outlook.com>
2021-03-22 15:43:49 +0100

    Add custom TransformerEncoder module

--- src/TextGAN_zoo/models/generator.py
+++ src/TextGAN_zoo/models/generator.py
@@ -10,6 +10,7 @@ import math
 
 import torch
 import torch.nn as nn
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
 import config as cfg
 from utils.helpers import truncated_normal_
@@ -107,3 +108,64 @@ class LSTMGenerator(nn.Module):
             return h.cuda(), c.cuda()
         else:
             return h, c
+
+
+class TransformerEncoder(nn.Module):
+
+    def __init__(self, embedding_dim, nhead, nhid, nlayers, dropout=0.5, vocab_size, max_seq_len, padding_idx, gpu=False):
+        super(TransformerEncoder, self).__init__()
+        self.model_type = 'TransformerEncoder'
+        # Compared to pytorch transformer_tutorial: ntoken = vocab_size    and  ninp= embedding_dim
+
+        self.embedding_dim = embedding_dim
+        self.max_seq_len = max_seq_len
+        self.vocab_size = vocab_size
+        self.padding_idx = padding_idx
+        self.gpu = gpu
+
+        self.encoder = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
+        self.pos_encoder = PositionalEncoding(embedding_dim, dropout)
+        encoder_layers = TransformerEncoderLayer(embedding_dim, nhead, nhid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers, )
+        
+        self.decoder = nn.Linear(embedding_dim, vocab_size)
+        self.softmax = nn.Softmax(dim=-1)
+
+        self.init_weights()
+
+    def generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src, src_mask):
+        src = self.encoder(src) * math.sqrt(self.embedding_dim)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.decoder(output)
+        output = self.softmax(output)
+        return output
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
