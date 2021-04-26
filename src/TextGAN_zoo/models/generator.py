95f007b95c318e205b6090a5cbfb5d70828d8ad1
Kevin Blin <kblin@outlook.com>
2021-04-26 12:54:41 +0200

    Debugging of tensor dimension in forward pass

--- src/TextGAN_zoo/models/generator.py
+++ src/TextGAN_zoo/models/generator.py
@@ -7,9 +7,10 @@
 # @Description  :
 # Copyrights (C) 2018. All Rights Reserved.
 import math
-
+import numpy as np
 import torch
 import torch.nn as nn
+import torch.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
 import config as cfg
@@ -126,13 +127,15 @@ class LSTMGenerator(nn.Module):
 # code adapted from https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 class TransformerGenerator(nn.Module):
 
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, max_seq_len, padding_idx, num_heads=2, nlayers=2, dropout=0.5, gpu=False):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, max_seq_len, padding_idx, nhead=2, nlayers=2, dropout=0.5, gpu=False):
         super(TransformerGenerator, self).__init__()
         self.model_type = 'TransformerGenerator'
         # Compared to pytorch transformer_tutorial: ntoken = vocab_size    and  ninp= embedding_dim  nhid=hidden_dim
 
-        self.hidden_dim = hidden_dim
-        self.embedding_dim = embedding_dim
+        self.embedding_dim = embedding_dim  # embedding dimension
+        self.hidden_dim = hidden_dim        # the dimension of the feedforward network model in nn.TransformerEncoder
+        self.nlayers = nlayers              # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+        self.nhead = nhead                  # the number of heads in the multiheadattention models
         self.max_seq_len = max_seq_len
         self.vocab_size = vocab_size
         self.padding_idx = padding_idx
@@ -140,8 +143,8 @@ class TransformerGenerator(nn.Module):
 
         self.encoder = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
         self.pos_encoder = PositionalEncoding(embedding_dim, dropout)
-        print(f"embed_dim: {embedding_dim}, hidden_dim: {hidden_dim}, num_heads:{num_heads}")
-        encoder_layers = TransformerEncoderLayer(embedding_dim, num_heads, hidden_dim, dropout)
+        print(f"embed_dim: {embedding_dim}, hidden_dim: {hidden_dim}, num_heads:{nhead}")
+        encoder_layers = TransformerEncoderLayer(embedding_dim, nhead, hidden_dim, dropout)
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
         
         self.decoder = nn.Linear(embedding_dim, vocab_size)
@@ -153,31 +156,39 @@ class TransformerGenerator(nn.Module):
         mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
+    
+    def subsequent_mask(self, size):
+        "Mask out subsequent positions."
+        attn_shape = (1, size, size)
+        subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+        return torch.from_numpy(subsequent_mask) == 0
 
-    def forward(self, inp, src_mask=None):
-        #print("SRC1:")
-        #print(inp.size())
-        src = self.encoder(inp) * math.sqrt(self.embedding_dim)
-        #print("SRC2:")
-        #print(src.size())
-        if len(inp.size()) == 1:
-            src = src.unsqueeze(1)  # batch_size * 1 * embedding_dim
-        src = self.pos_encoder(src)
-        #print("SRC3:")
-        #print(src.size())
-        output = self.transformer_encoder(src, src_mask)
+    def forward(self, src, src_mask=None):
+        print("1. SRC:")
+        print(src.size())
+        src1 = self.encoder(src) * math.sqrt(self.embedding_dim)
+        print("2. SRC after encoder(inp):")
+        print(src1.size())
+        if len(src1.size()) == 1:
+            src1 = src1.unsqueeze(1)  # batch_size * 1 * embedding_dim
+        print("3. SRC after unsqueeze:")
+        print(src1.size())
+        src2 = self.pos_encoder(src1)
+        print("4. SRC after pos_encoder:")
+        print(src2.size())
+        output = self.transformer_encoder(src2, src_mask)
         #output = self.transformer_encoder(src)
-        #print("OUT1:")
-        #print(output.size())
-        output = output.contiguous().view(-1, self.hidden_dim)  # out: (batch_size * len) * hidden_dim
-        #print("OUT2:")
-        #print(output.size())
+        print("1. OUT after transencod:")
+        print(output.size())
         output = self.decoder(output)
-        #print("OUT2: ")
-        #print(output.size())
+        print("2. OUT after decoder: ")
+        print(output.size())
+        output = output.contiguous().view(-1, self.hidden_dim)  # out: (batch_size * len) * hidden_dim
+        print("3. OUT after view:")
+        print(output.size())
         pred = self.softmax(output)
-        #print("PRED")
-        #print(pred.size())
+        print("PRED")
+        print(pred.size())
         return pred
 
 
@@ -200,15 +211,26 @@ class TransformerGenerator(nn.Module):
         # Generate sentences with multinomial sampling strategy
         for b in range(num_batch):
             #inp = torch.LongTensor([start_letter] * batch_size)
-            inp = torch.LongTensor(self.max_seq_len)
+            #inp = torch.LongTensor([start_letter] * self.max_seq_len)
+            inp = torch.LongTensor([start_letter] * self.max_seq_len)
+            inp = inp.unsqueeze(1).expand(20, batch_size)
+            print(f"start letter:, {[start_letter]}")
+            print(f"batch size: {batch_size}")
+            print(f"inp:, {inp}")
+            #inp = torch.LongTensor(self.max_seq_len)
             if self.gpu:
                 inp = inp.cuda()
 
             for i in range(self.max_seq_len):
+                print(f"inp in range: {inp.size()}")
+
                 out = self.forward(inp, self.generate_square_subsequent_mask(self.max_seq_len))  # out: batch_size * vocab_size
                 #out = self.forward(inp)  # out: batch_size * vocab_size
-                #print(out.size())
+                print(f"out: {out.size()}")
                 next_token = torch.multinomial(torch.exp(out), 1)  # batch_size * 1 (sampling from each row)
+                print(f"Nexttoken: {next_token.size()}")
+                print(f"samples: {samples.size()}")
+
                 samples[b * batch_size:(b + 1) * batch_size, i] = next_token.view(-1)
                 inp = next_token.view(-1)
         samples = samples[:num_samples]
@@ -227,20 +249,33 @@ class TransformerGenerator(nn.Module):
                 initrange = 0.1
                 torch.nn.init.uniform_(param, -initrange, initrange)
 
+class Generator(nn.Module):
+        "Define standard linear + softmax generation step."
+        def __init__(self, d_model, vocab):
+            super(Generator, self).__init__()
+            self.proj = nn.Linear(d_model, vocab)
+
+        def forward(self, x):
+            return F.log_softmax(self.proj(x), dim=-1)
+
 
 class PositionalEncoding(nn.Module):
-
+    "Implement the PE function."
     def __init__(self, d_model, dropout=0.1, max_len=5000):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
+
+        # Compute the positional encodings once in log space.
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
+        #pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
         x = x + self.pe[:x.size(0), :]
+        #x = x + Variable(self.pe[:, :x.size(1)], requires_grad=False)
         return self.dropout(x)
