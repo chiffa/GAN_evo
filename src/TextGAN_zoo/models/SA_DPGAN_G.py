04b7f2075e36ba4aa235bce06e12c7448c01d546
TheBlueHawk <kblin@outlook.com>
2021-05-04 09:47:00 +0200

    Fix sample_teacher_forcing input dimension order

--- src/TextGAN_zoo/models/SA_DPGAN_G.py
+++ src/TextGAN_zoo/models/SA_DPGAN_G.py
@@ -20,8 +20,8 @@ class SA_DPGAN_G(TransformerGenerator):
         """
         batch_size, _ = inp.size()
         src_mask = self.generate_square_subsequent_mask(self.max_seq_len)
-
-        #pred = self.forward(inp, src_mask)
+        inp = inp.transpose(1, 0)       # [max_seq_len, batch_size]
+        print(f"inp: {inp.size()}")
         pred = self.forward(inp, src_mask)
         pred = pred.view(-1, self.vocab_size) # [max_seq_len * batch_size, vocab_size]
         #print(f"pred: {pred.size()}")
@@ -29,5 +29,4 @@ class SA_DPGAN_G(TransformerGenerator):
         samples = torch.argmax(pred, dim=-1).view(batch_size, -1)
         log_prob = F.nll_loss(pred, samples.view(-1), reduction='none').view(batch_size, -1)
         # samples = torch.multinomial(torch.exp(log_prob), 1)
-
         return samples, log_prob
